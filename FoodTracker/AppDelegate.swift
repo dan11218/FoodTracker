f04acece803509865488eb4a25790b933e792544
Danny Graugnard <dangraug@me.com>
2018-08-21 15:30:48 -0400

    imports Usabilla SDK

--- FoodTracker/AppDelegate.swift
+++ FoodTracker/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+import Usabilla
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -15,10 +17,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        Usabilla.initialize(appID: "badcf3a9-d07c-4ad5-88cd-0c7c3bf2bf69")
+        
+        //Your form ID is: 5b2c08070f37bb701b5625fd
+        
         // Override point for customization after application launch.
         return true
     }
-
+   
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
